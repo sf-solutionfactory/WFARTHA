0f7dd0129cc63a86ce9cef63ab7d00173e03edbf
enrique-sf <enrique.gonzalez@sf-solutionfactory.com>
2018-10-25 09:20:41 -0500

    Modificas a edit
    Inicios de codigo logica edit

--- WFARTHA/Scripts/editArtha.js
+++ WFARTHA/Scripts/editArtha.js
@@ -1,4 +1,5 @@
-﻿var tRet2 = [];
+﻿var tRet = [];//Agrego a un array los tipos de retenciones
+var tRet2 = [];
 $(document).ready(function () {
     var elem = document.querySelectorAll('select');
     var instance = M.Select.init(elem, []);
@@ -11,13 +12,299 @@ $(document).ready(function () {
 
     solicitarDatos();
     $('#btn_guardarh').on("click", function (e) {
+        //Guardar los valores de la tabla en el modelo para enviarlos al controlador
+        copiarTableInfoControl(); //copiarTableInfoPControl();
+        //copiarTableSopControl();
+        copiarTableRet();
+
+        //CODIGO
+        //dar formato al monto
+        var enca_monto = $("#MONTO_DOC_MD").val();
+        enca_monto = enca_monto.replace(/\s/g, '');
+        //enca_monto = toNum(enca_monto);
+        //enca_monto = parseFloat(enca_monto);
+        $("#MONTO_DOC_MD").val(enca_monto);
+
+        //LEJ 11.09.2018
+        //dar formato al T CAMBIO
+        var tcambio = $("#TIPO_CAMBIO").val();
+        tcambio = tcambio.replace(/\s/g, '');
+        tcambio = toNum(tcambio);
+        tcambio = parseFloat(tcambio);
+        $("#TIPO_CAMBIO").val(tcambio);
 
         //Termina provisional
         $('#btn_guardar').click();
-
-
     });
 });
+
+function copiarTableInfoControl() {
+
+    var lengthT = $("table#table_info tbody tr[role='row']").length;
+    var docsenviar = {};
+    var docsenviar2 = {};
+    var docsenviar3 = {};//lej01.10.2018
+    if (lengthT > 0) {
+        //Obtener los valores de la tabla para agregarlos a la tabla oculta y agregarlos al json
+        //Se tiene que jugar con los index porque las columnas (ocultas) en vista son diferentes a las del plugin
+        jsonObjDocs = [];
+        jsonObjDocs2 = [];
+        jsonObjDocs3 = [];//lej01.10.2018
+        var i = 1;
+        var t = $('#table_info').DataTable();
+        //Lej 14.09.18---------------------
+        //Aqui armo la tabla oculta de acuerdo a los valores y columnas ingresados de retencion
+        var taInf = $("#table_inforeth");
+        taInf.append($("<thead />"));
+        taInf.append($("<tbody />"));
+        var thead = $("#table_inforeth thead");
+        thead.append($("<tr />"));
+        //for (y = 0; y < tRet2.length; y++) {
+        //$("#table_inforeth>thead>tr").append("<th>" + tRet2[i] + " B.Imp.</th>");//Base imponible
+        //$("#table_inforeth>thead>tr").append("<th>" + tRet2[i] + " I. Ret.</th>");//Imp Ret
+        $("#table_inforeth>thead>tr").append("<th>WITHT</th>");
+        $("#table_inforeth>thead>tr").append("<th>WT_WITHCD</th>");
+        $("#table_inforeth>thead>tr").append("<th>I. Ret.</th>");//Imp Ret
+        $("#table_inforeth>thead>tr").append("<th>B.Imp.</th>");//Base imponible
+        // }
+        //Lej 14.09.18----------------
+        $("#table_info > tbody  > tr[role='row']").each(function () {
+
+            //Obtener el row para el plugin
+            var tr = $(this);
+            var indexopc = t.row(tr).index();
+
+            var tconcepto = "";
+            //Obtener el concepto
+            var inpt = t.row(indexopc).data()[9];
+            //LEJ 03-10-2018
+            if (inpt !== "") {
+                var parser = $($.parseHTML(inpt));
+                tconcepto = parser.val();
+            }
+            else {
+                tconcepto = "";
+            }
+            //LEJ 03-10-2018
+            //MGC 11-10-2018 Obtener valor de columnas ocultas --------------------------->
+            //Obtener la cuenta
+            var cuenta = t.row(indexopc).data()[11];
+
+            //Obtener la imputación
+            var imputacion = t.row(indexopc).data()[14];
+
+            //MGC 22-10-2018 Modificación en etiquetas
+            //Obtener el nombre de la cuenta
+            var cuentanom = t.row(indexopc).data()[12];
+
+            //MGC 11-10-2018 Obtener valor de columnas ocultas <---------------------------
+            //Lej 14.08.2018-------------------------------------------------------------I
+            var colsAdded = tRet2.length;//Las retenciones que se agregaron a la tabla
+            var retTot = tRet.length;//Todas las retenciones
+            //Lej 14.08.2018-------------------------------------------------------------T
+            var pos = toNum($(this).find("td.POS").text());
+            // var ca = $(this).find("td.CA").text(); //MGC 04092018 Conceptos
+            var ca = t.row(indexopc).data()[7];//lejgg 09-10-2018 Conceptos
+            var factura = $(this).find("td.FACTURA input").val();
+            //var tconcepto = $(this).find("td.TCONCEPTO").text();
+            var grupo = $(this).find("td.GRUPO input").val();
+
+            //quitar espacios en blanco //MGC 22-10-2018 Modificación en etiquetas
+            grupo = grupo.replace(/\s/g, '');
+            var grupoaux = grupo;
+            grupo = "";
+
+            //Quitar el tipo de concepto de la llave
+            grupo = grupoaux.substring(2, grupoaux.length);
+
+            //var cuenta = $(this).find("td.CUENTA").text();//MGC 04092018 Conceptos //MGC 11-10-2018 Obtener valor de columnas oculta
+            //var cuentanom = $(this).find("td.CUENTANOM").text();//MGC 22-10-2018 Modificación en etiquetas
+            //var tipoimp = $(this).find("td.TIPOIMP").text();//MGC 22-10-2018 Modificación en etiquetas
+            var tipoimp = t.row(indexopc).data()[13];//MGC 22-10-2018 Modificación en etiquetas
+
+            //var imputacion = $(this).find("td.IMPUTACION").text(); //MGC 11-10-2018 Obtener valor de columnas oculta
+            var ccosto = $(this).find("td.CCOSTO input").val(); //MGC 11-10-2018 Obtener valor de columnas oculta
+            var impuesto = $(this).find("td.IMPUESTO input").val();
+            var monto1 = $(this).find("td.MONTO input").val();
+            monto1 = monto1.replace(/\s/g, '');
+            var monto = toNum(monto1);
+            var iva1 = $(this).find("td.IVA input").val();
+            iva1 = iva1.replace(/\s/g, '');
+            var iva = toNum(iva1);
+            var total1 = $(this).find("td.TOTAL input").val();
+            var texto = $(this).find("td.TEXTO input").val();//LEJ 14.09.2018
+            total1 = total1.replace(/\s/g, '');
+            var total = toNum(total1);
+            //Para anexos
+            //-----------------------
+
+            var item3 = {};
+            var an1 = $(this).find("td.NumAnexo input").val();
+            var an2 = $(this).find("td.NumAnexo2 input").val();
+            var an3 = $(this).find("td.NumAnexo3 input").val();
+            var an4 = $(this).find("td.NumAnexo4 input").val();
+            var an5 = $(this).find("td.NumAnexo5 input").val();
+            item3["a1"] = an1;
+            item3["a2"] = an2;
+            item3["a3"] = an3;
+            item3["a4"] = an4;
+            item3["a5"] = an5;
+            jsonObjDocs3.push(item3);
+            item3 = "";
+            //-----------------------
+            for (j = 0; j < tRet2.length; j++) {
+                //var xvl = $(this).find("td.BaseImp" + tRet2[j] + " input").val();
+                //baseImp.push($(this).find("td.BaseImp" + tRet2[j] + " input").val());//LEJ 14.09.2018
+                //ImpRet.push($(this).find("td.ImpRet" + tRet2[j] + " input").val());//LEJ 14.09.2018
+                //llenare mis documentorp's
+                var item2 = {};
+                item2["NUM_DOC"] = 0;
+                item2["POS"] = pos;
+                item2["WITHT"] = tRet2[j];
+                item2["WT_WITHCD"] = "01";
+                item2["BIMPONIBLE"] = parseFloat($(this).find("td.BaseImp" + tRet2[j] + " input").val().replace('$', '').replace(',', ''));
+                item2["IMPORTE_RET"] = parseFloat($(this).find("td.ImpRet" + tRet2[j] + " input").val().replace('$', '').replace(',', ''));
+                jsonObjDocs2.push(item2);
+                item2 = "";
+            }
+
+            var item = {};
+
+            item["NUM_DOC"] = 0;
+            item["POS"] = pos;
+            item["ACCION"] = ca;
+            item["FACTURA"] = factura;
+            item["TCONCEPTO"] = tconcepto;
+            item["GRUPO"] = grupo;
+            item["CUENTA"] = cuenta;
+            item["NOMCUENTA"] = cuentanom;
+            item["TIPOIMP"] = tipoimp;
+            item["IMPUTACION"] = imputacion;
+            item["CCOSTO"] = ccosto;
+            item["MONTO"] = monto;
+            item["MWSKZ"] = impuesto;
+            item["IVA"] = iva;
+            item["TEXTO"] = texto;
+            item["TOTAL"] = total;
+
+            jsonObjDocs.push(item);
+            i++;
+            item = "";
+
+        });
+
+        docsenviar = JSON.stringify({ 'docs': jsonObjDocs });
+
+        $.ajax({
+            type: "POST",
+            url: 'getPartialCon',
+            contentType: "application/json; charset=UTF-8",
+            data: docsenviar,
+            success: function (data) {
+
+                if (data !== null || data !== "") {
+
+                    $("table#table_infoh tbody").append(data);
+                }
+
+            },
+            error: function (xhr, httpStatusMessage, customErrorMessage) {
+                M.toast({ html: httpStatusMessage });
+            },
+            async: false
+        });
+
+        docsenviar2 = JSON.stringify({ 'docs': jsonObjDocs2 });
+
+        //Ajax para las retenciones en la tabla de info
+        $.ajax({
+            type: "POST",
+            url: 'getPartialCon2',
+            contentType: "application/json; charset=UTF-8",
+            data: docsenviar2,
+            success: function (data) {
+
+                if (data !== null || data !== "") {
+
+                    $("table#table_inforeth tbody").append(data);
+                }
+
+            },
+            error: function (xhr, httpStatusMessage, customErrorMessage) {
+                M.toast({ html: httpStatusMessage });
+            },
+            async: false
+        });
+
+        docsenviar3 = JSON.stringify({ 'docs': jsonObjDocs3 });
+        $.ajax({
+            type: "POST",
+            url: 'getPartialCon3',
+            contentType: "application/json; charset=UTF-8",
+            data: docsenviar3,
+            success: function (data) {
+
+                if (data !== null || data !== "") {
+
+                    $("table#table_infoAnex tbody").append(data);
+                }
+
+            },
+            error: function (xhr, httpStatusMessage, customErrorMessage) {
+                M.toast({ html: httpStatusMessage });
+            },
+            async: false
+        });
+
+    }
+
+}
+
+function copiarTableSopControl() {
+    var lengthT = $("table#table_sop tbody tr[role='row']").length;
+
+    if (lengthT > 0) {
+        //Obtener los valores de la tabla para agregarlos a la tabla oculta y agregarlos al json
+        //Se tiene que jugar con los index porque las columnas (ocultas) en vista son diferentes a las del plugin
+        jsonObjDocs = [];
+        var i = 1;
+
+
+        $("#table_sop > tbody  > tr[role='row']").each(function () {
+
+            //Obtener el id de la categoría            
+            var t = $('#table_sop').DataTable();
+            var tr = $(this);
+
+            var indexopc = t.row(tr).index();
+            var opc = t.row(indexopc).data()[1];
+        });
+
+        docsenviar = JSON.stringify({ 'docs': jsonObjDocs });
+
+        $.ajax({
+            type: "POST",
+            url: 'getPartialDis',
+            contentType: "application/json; charset=UTF-8",
+            data: docsenviar,
+            success: function (data) {
+
+                if (data !== null || data !== "") {
+
+                    $("table#table_dish tbody").append(data);
+                    if (borrador != "X") { //B20180625 MGC 2018.07.03
+                        $('#delRow').click();
+                    }
+                }
+
+            },
+            error: function (xhr, httpStatusMessage, customErrorMessage) {
+                M.toast({ html: httpStatusMessage });
+            },
+            async: false
+        });
+    }
+}
 
 function solicitarDatos() {
     var _ref = $('#REFERENCIA').val();
@@ -328,25 +615,32 @@ function armarTablaInfo(datos) {
     });
     var _infoc = _infoBIIR.length / 2;
     var arrColExTA = [];
-    if (_infoAnex.length === datos.DOCUMENTOPSTR.length) {
-        if (_infoc === datos.DOCUMENTOPSTR.length) {
-            for (var i = 0; i < datos.DOCUMENTOPSTR.length; i++) {
-                for (var x = 0; x < _infoBIIR.length; x++) {
-                    if (_infoBIIR[x].POS === (i + 1)) {
-                        arrColExTA.push(_infoBIIR[x]);
-                    }
+
+
+    if (_infoc === datos.DOCUMENTOPSTR.length) {
+        for (var i = 0; i < datos.DOCUMENTOPSTR.length; i++) {
+            for (var x = 0; x < _infoBIIR.length; x++) {
+                if (_infoBIIR[x].POS === (i + 1)) {
+                    arrColExTA.push(_infoBIIR[x]);
                 }
-                if (datos.DOCUMENTOPSTR[i].TEXTO === null) {
-                    datos.DOCUMENTOPSTR[i].TEXTO = "";
-                }
-                if (datos.DOCUMENTOPSTR[i].IMPUTACION === null) {
-                    datos.DOCUMENTOPSTR[i].IMPUTACION = "";
-                }
+            }
+            if (datos.DOCUMENTOPSTR[i].TEXTO === null) {
+                datos.DOCUMENTOPSTR[i].TEXTO = "";
+            }
+            if (datos.DOCUMENTOPSTR[i].IMPUTACION === null) {
+                datos.DOCUMENTOPSTR[i].IMPUTACION = "";
+            }
+            if (_infoAnex.length > 0) {
                 addRowInfo($('#table_info').DataTable(), datos.DOCUMENTOPSTR[i].POS, _infoAnex[i].a1, _infoAnex[i].a2, _infoAnex[i].a3, _infoAnex[i].a4, _infoAnex[i].a5, datos.DOCUMENTOPSTR[i].ACCION, datos.DOCUMENTOPSTR[i].FACTURA, "", datos.DOCUMENTOPSTR[i].GRUPO, datos.DOCUMENTOPSTR[i].CUENTA,
+                    datos.DOCUMENTOPSTR[i].NOMCUENTA, datos.DOCUMENTOPSTR[i].TIPOIMP, datos.DOCUMENTOPSTR[i].IMPUTACION, "", datos.DOCUMENTOPSTR[i].MONTO, "", datos.DOCUMENTOPSTR[i].IVA, datos.DOCUMENTOPSTR[i].TEXTO, datos.DOCUMENTOPSTR[i].TOTAL, "", "", arrColExTA);
+            }
+            else {
+                addRowInfo($('#table_info').DataTable(), datos.DOCUMENTOPSTR[i].POS, "", "", "", "", "", datos.DOCUMENTOPSTR[i].ACCION, datos.DOCUMENTOPSTR[i].FACTURA, "", datos.DOCUMENTOPSTR[i].GRUPO, datos.DOCUMENTOPSTR[i].CUENTA,
                     datos.DOCUMENTOPSTR[i].NOMCUENTA, datos.DOCUMENTOPSTR[i].TIPOIMP, datos.DOCUMENTOPSTR[i].IMPUTACION, "", datos.DOCUMENTOPSTR[i].MONTO, "", datos.DOCUMENTOPSTR[i].IVA, datos.DOCUMENTOPSTR[i].TEXTO, datos.DOCUMENTOPSTR[i].TOTAL, "", "", arrColExTA);
             }
         }
     }
+
 }
 
 function addRowInfo(t, POS, NumAnexo, NumAnexo2, NumAnexo3, NumAnexo4, NumAnexo5, CA, FACTURA, TIPO_CONCEPTO, GRUPO, CUENTA, CUENTANOM, TIPOIMP, IMPUTACION, CCOSTO, MONTO, IMPUESTO, IVA, TEXTO, TOTAL, disabled, check, colsBIIR) { //MGC 03 - 10 - 2018 solicitud con orden de compra
